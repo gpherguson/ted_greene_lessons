3d23dbf79934ac87a2e286c18e2bcaaa050c746e
Greg Ferguson <8si.greg@gmail.com>
2013-04-16 21:01:41 -0700

    fixed blankline problem. added more exception handling

--- get_ted_greene_lessons.rb
+++ get_ted_greene_lessons.rb
@@ -18,24 +18,30 @@ Dir.chdir(BASE_DIR) do |dir|
     lesson_page = Nokogiri::HTML(open(lesson_page_uri))
     lesson_page.search('td.verdana12spaced table td').each_slice(2) do |name_td, url_td|
 
-      pdf_url = lesson_page_uri + URI.escape(url_td.at('a')['href'])
+      url_td_a = url_td.at('a')
+      next unless url_td_a
+
+      pdf_url = lesson_page_uri + URI.escape(url_td_a['href'])
       pdf_filename = URI.unescape(File.basename(pdf_url.to_s))
 
       print "Reading: #{ pdf_filename }... "
       if File.exists?(pdf_filename)
         puts "already exists."
-      else
-        begin
-          start_time = Time.now
-          File.write(pdf_filename, open(pdf_url).read)
-          end_time = Time.now
-          File.open('Ted Green lessons.txt', 'a') do |fo|
-            fo.puts "#{ name_td.text }\t#{ pdf_filename }"
-          end
-          puts "finished in #{ end_time - start_time } seconds."
-        rescue Timeout::Error => e
-          puts "timed out."
+        next
+      end
+
+      begin
+        start_time = Time.now
+        File.write(pdf_filename, open(pdf_url).read)
+        end_time = Time.now
+        File.open('Ted Green lessons.txt', 'a') do |fo|
+          fo.puts "#{ name_td.text }\t#{ pdf_filename }"
         end
+        puts "finished in %0.2f seconds." % (end_time - start_time)
+      rescue Timeout::Error => e
+        puts "timed out."
+      rescue Exception => e
+        STDERR.puts e.message
       end
 
     end
